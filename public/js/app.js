a3c887824dc9cd09b33daab06fde2a7a417ff75b
leiwen <leiwen@szqgly.com>
2019-10-17 14:56:30 +0800

    :heart:修改地图上显示的图标

--- public/js/app.js
+++ public/js/app.js
@@ -1838,6 +1838,7 @@ module.exports = {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _config_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../config.js */ "./resources/js/config.js");
 //
 //
 //
@@ -1851,6 +1852,7 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+
 /* harmony default export */ __webpack_exports__["default"] = ({
   props: {
     'latitude': {
@@ -1897,13 +1899,23 @@ __webpack_require__.r(__webpack_exports__);
     // 为所有咖啡店创建点标记
     buildMarkers: function buildMarkers() {
       // 清空点标记数组
-      this.markers = []; // 遍历所有咖啡店并为每个咖啡店创建点标记
+      this.markers = []; // 自定义点标记图标
+
+      var image = _config_js__WEBPACK_IMPORTED_MODULE_0__["ROAST_CONFIG"].APP_URL + '/storage/img/coffee-marker.png';
+      var icon = new AMap.Icon({
+        image: image,
+        // 图像 URL
+        imageSize: new AMap.Size(19, 33) // 设置图标尺寸
+
+      }); // 遍历所有咖啡店并为每个咖啡店创建点标记
 
       for (var i = 0; i < this.cafes.length; i++) {
         // 通过高德地图 API 为每个咖啡店创建点标记并设置经纬度
         var marker = new AMap.Marker({
           position: new AMap.LngLat(parseFloat(this.cafes[i].latitude), parseFloat(this.cafes[i].longitude)),
-          title: this.cafes[i].name
+          title: this.cafes[i].name,
+          icon: icon // 通过 icon 对象设置自定义点标记图标来替代默认蓝色图标
+
         }); // 将每个点标记放到点标记数组中
 
         this.markers.push(marker);
@@ -61071,20 +61083,24 @@ __webpack_require__.r(__webpack_exports__);
  * Defines the API route we are using.
  */
 var api_url = '';
+var app_url = '';
 var gaode_maps_js_api_key = 'f1c1c7f70d846eadc851d3840f937b5e';
 
 switch ("development") {
   case 'development':
     api_url = 'http://homestead.test/api/v1';
+    app_url = 'http://homestead.test';
     break;
 
   case 'production':
     api_url = 'http://homestead.demo.laravelacademy.org/api/v1';
+    app_url = 'http://homestead.demo.laravelacademy.org';
     break;
 }
 
 var ROAST_CONFIG = {
   API_URL: api_url,
+  APP_URL: app_url,
   GAODE_MAPS_JS_API_KEY: gaode_maps_js_api_key
 };
 
--- resources/js/components/cafes/CafeMap.vue
+++ resources/js/components/cafes/CafeMap.vue
@@ -12,6 +12,7 @@
 </template>
 
 <script>
+    import {ROAST_CONFIG} from '../../config.js';
     export default {
         props: {
             'latitude': {  // 经度
@@ -57,13 +58,21 @@
                 // 清空点标记数组
                 this.markers = [];
 
+                // 自定义点标记图标
+                var image = ROAST_CONFIG.APP_URL + '/storage/img/coffee-marker.png';
+                var icon = new AMap.Icon({
+                    image: image,  // 图像 URL
+                    imageSize: new AMap.Size(19, 33)  // 设置图标尺寸
+                });
+
                 // 遍历所有咖啡店并为每个咖啡店创建点标记
                 for (var i = 0; i < this.cafes.length; i++) {
 
                     // 通过高德地图 API 为每个咖啡店创建点标记并设置经纬度
                     var marker = new AMap.Marker({
                         position: new AMap.LngLat(parseFloat(this.cafes[i].latitude), parseFloat(this.cafes[i].longitude)),
-                        title: this.cafes[i].name
+                        title: this.cafes[i].name,
+                        icon: icon,  // 通过 icon 对象设置自定义点标记图标来替代默认蓝色图标
                     });
 
                     // 将每个点标记放到点标记数组中
